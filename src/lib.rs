08cf9b0ab2686ea859f52a41d6d218495956cb59
hazelnutsgz <15201752137@163.com>
2020-01-16 15:02:10 +0800

    Fix

--- src/lib.rs
+++ src/lib.rs
@@ -28,121 +28,137 @@ pub struct Matches<'a, 'b> {
 }
 
 impl<'a, 'b> CliParser<'a, 'b> {
-
     pub fn new() -> Self {
         CliParser {
-            rules: HashMap::new()
+            rules: HashMap::new(),
         }
     }
 
-    pub fn add_rule_with_default(&mut self, name: &'a str, rule_type: RuleType, default_value: &'b str) -> Result<(), &'static str> {
+    pub fn add_rule_with_default(
+        &mut self,
+        name: &'a str,
+        rule_type: RuleType,
+        default_value: &'b str,
+    ) -> Result<(), &'static str> {
         match rule_type {
             RuleType::Maybe => {
-                self.rules.insert(name, Rule {
-                    rule_type: RuleType::Maybe,
-                    default_value: Some(default_value),
-                });
+                self.rules.insert(
+                    name,
+                    Rule {
+                        rule_type: RuleType::Maybe,
+                        default_value: Some(default_value),
+                    },
+                );
                 Ok(())
-            },
+            }
             RuleType::NoArg => {
-                self.rules.insert(name, Rule {
-                    rule_type: RuleType::NoArg,
-                    default_value: Some(default_value),
-                });
+                self.rules.insert(
+                    name,
+                    Rule {
+                        rule_type: RuleType::NoArg,
+                        default_value: Some(default_value),
+                    },
+                );
                 Ok(())
-            },
-            RuleType::HasArg => {
-                Err("Default value is meaningless!")
-            },
+            }
+            RuleType::HasArg => Err("Default value is meaningless!"),
         }
     }
 
     pub fn add_rule(&mut self, name: &'a str) -> Result<&'static str, ()> {
-        self.rules.insert(name, Rule {
-            rule_type: RuleType::HasArg,
-            default_value: None
-        });
+        self.rules.insert(
+            name,
+            Rule {
+                rule_type: RuleType::HasArg,
+                default_value: None,
+            },
+        );
         Ok("Rule added")
     }
- 
-    pub fn parse(& self, arg_list: &'a Vec<String>) -> Result<Matches, &'static str> {
-        let mut opts : HashMap<&str, Opt> = HashMap::new();
 
-        println!("{:?}", arg_list);
+    pub fn parse(&self, arg_list: &'a Vec<String>) -> Result<Matches, &'static str> {
+        let mut opts: HashMap<&str, Opt> = HashMap::new();
+
         if arg_list.len() < 2 {
             return Err("No opt to parse!");
         } else {
             let mut index: usize = 1;
             while index < arg_list.len() {
-                if arg_list[index].chars().nth(0).unwrap() != '+' { return Err("The input contains no flag"); }
+                if arg_list[index].chars().nth(0).unwrap() != '+' {
+                    return Err("The input contains no flag");
+                }
 
                 match self.rules.get(&arg_list[index][1..]) {
                     Some(rule) => {
                         match rule.rule_type {
                             RuleType::NoArg => {
-                                opts.insert(&arg_list[index][1..], Opt {
-                                    argument: None,
-                                    rule_type: RuleType::NoArg,
-                                });
-                            },
-                            RuleType::HasArg => {
-                                if index + 1 >= arg_list.len() { return Err("Index out of bound"); }
-                                if arg_list[index+1].chars().nth(0).unwrap() == '+' { return Err("Need argument!"); }
-                                
-                                opts.insert(&arg_list[index][1..], Opt {
-                                    argument: Some(&arg_list[index+1]),
-                                    rule_type: rule.rule_type,
-                                });
-                                index += 1; // Eat it
-                            },
-                            RuleType::Maybe => {
-                                if index + 1 >= arg_list.len() { 
-                                    break;
-                                }
-                                if arg_list[index+1].chars().nth(0).unwrap() == '+' {
-                                    opts.insert(&arg_list[index][1..], Opt {
+                                opts.insert(
+                                    &arg_list[index][1..],
+                                    Opt {
                                         argument: None,
                                         rule_type: RuleType::NoArg,
-                                    });
-                                } else {
-                                    opts.insert(&arg_list[index][1..], Opt {
-                                        argument: Some(&arg_list[index+1]),
+                                    },
+                                );
+                            }
+                            RuleType::HasArg => {
+                                if index + 1 >= arg_list.len() {
+                                    return Err("Need argument: Index out of bound");
+                                }
+                                if arg_list[index + 1].chars().nth(0).unwrap() == '+' {
+                                    return Err("Need argument!");
+                                }
+
+                                opts.insert(
+                                    &arg_list[index][1..],
+                                    Opt {
+                                        argument: Some(&arg_list[index + 1]),
                                         rule_type: rule.rule_type,
-                                    });
+                                    },
+                                );
+                                index += 1; // Eat it
+                            }
+                            RuleType::Maybe => {
+                                if index + 1 >= arg_list.len()
+                                    || arg_list[index + 1].chars().nth(0).unwrap() == '+'
+                                {
+                                    opts.insert(
+                                        &arg_list[index][1..],
+                                        Opt {
+                                            argument: None,
+                                            rule_type: RuleType::Maybe,
+                                        },
+                                    );
+                                } else {
+                                    opts.insert(
+                                        &arg_list[index][1..],
+                                        Opt {
+                                            argument: Some(&arg_list[index + 1]),
+                                            rule_type: RuleType::Maybe,
+                                        },
+                                    );
                                     index += 1; // Eat it
                                 }
-                            },
+                            }
                         };
-                    },
-                    _ => {
-                        return Err("Cannot recognize rule")
                     }
+                    _ => return Err("Cannot recognize rule"),
                 };
                 index += 1;
             }
         }
 
-        Ok(Matches {
-            opts: opts
-        })
+        Ok(Matches { opts: opts })
     }
 }
 
 impl<'a, 'b> Matches<'a, 'b> {
     pub fn get_opt(&self, opt_str: &'b str) -> Result<Option<&'b str>, &'a str> {
-        for (contact, &number) in self.opts.iter() { 
-            println!("{}", contact);
-        }
         match self.opts.get(opt_str) {
-            Some(target_opt) => {
-                match target_opt.rule_type {
-                    RuleType::NoArg => Ok(None),
-                    _ => Ok(target_opt.argument),
-                }
+            Some(target_opt) => match target_opt.rule_type {
+                RuleType::NoArg => Ok(None),
+                _ => Ok(target_opt.argument),
             },
-            _ => {
-                Err("No argument found")
-            }
+            _ => Err("No argument found"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use CliParse::{
-    CliParser, Matches
-};
 use std::env;
+use CliParse::{CliParser, Matches, RuleType};
 fn main() {
     let mut cli_parser = CliParser::new();
-    cli_parser.add_rule("a");
+    cli_parser.add_rule_with_default("a", RuleType::Maybe, "asdf");
     cli_parser.add_rule("b");
     cli_parser.add_rule("c");
     cli_parser.add_rule("d");
@@ -12,5 +10,5 @@ fn main() {
     let arg_list: Vec<String> = env::args().collect();
     let matches: Matches = cli_parser.parse(&arg_list).unwrap();
 
-    matches.get_opt("h").unwrap();
+    matches.get_opt("a").unwrap();
 }
