934f2fd623322f7a329963244e547271094925ee
hazelnutsgz <15201752137@163.com>
2020-01-16 14:15:59 +0800

    Version 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use CliParse::{
+    CliParser, Matches
+};
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let mut cli_parser = CliParser::new();
+    cli_parser.add_rule("a");
+    cli_parser.add_rule("b");
+    cli_parser.add_rule("c");
+    cli_parser.add_rule("d");
+    cli_parser.add_rule("h");
+    let arg_list: Vec<String> = env::args().collect();
+    let matches: Matches = cli_parser.parse(&arg_list).unwrap();
+
+    matches.get_opt("h").unwrap();
 }
